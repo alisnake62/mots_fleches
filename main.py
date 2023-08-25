5e2926610aa766d5bdf5a0f9578b31217859f578
clement sergent <clement.sergent@corp.ovh.com>
2023-08-25 08:53:02 +0200

    refacto

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ import datetime
 
 args = sys.argv
 
-if len(args) == 1:
-    gameId = "250823"
-else:
-    gameId = args[1]
+input_date = None if len(args) == 1 else args[1]
 
-formated_date = gameId[:4] + '20' + gameId[4:]
-date_str = datetime.datetime.strptime(formated_date, "%d%m%Y").strftime("%d %B %Y")
+input_date = datetime.datetime.strptime(input_date, "%d%m%Y") if input_date is not None else datetime.datetime.today()
+
+formated_date = input_date.strftime("%d%m%Y")
+game_id = formated_date[:4] + formated_date[6:]
+
+display_date = input_date.strftime("%d %B %Y")
 
 options = Options()
 options.headless = False
@@ -28,14 +29,14 @@ def find_between( s, first, last ):
     except ValueError:
         return ""
 
-gameDataRequest = requests.get(f"https://rcijeux.fr/drupal_game/20minutes/grids/{gameId}.mfj")
-gameData = str(gameDataRequest.content)
-force = find_between(gameData, "force:\"", "\",\\r")
+game_data_dequest = requests.get(f"https://rcijeux.fr/drupal_game/20minutes/grids/{game_id}.mfj")
+game_data = str(game_data_dequest.content)
+force = find_between(game_data, "force:\"", "\",\\r")
 
 driver = Firefox(options=options)
-driver.get(f"https://rcijeux.fr/game/20minutes/mfleches?id={gameId}")
+driver.get(f"https://rcijeux.fr/game/20minutes/mfleches?id={game_id}")
 
 title_element = driver.find_element(By.ID, "game-name")
-driver.execute_script(f"document.getElementById('game-name').innerHTML = '{date_str} - Force {force}';")
+driver.execute_script(f"document.getElementById('game-name').innerHTML = '{display_date} - Force {force}';")
 
 # driver.close()
