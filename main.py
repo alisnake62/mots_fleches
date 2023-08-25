9193020952d6f1c16147a370b7efd7a753888241
clement sergent <clement.sergent@corp.ovh.com>
2023-08-25 08:44:21 +0200

    add date on title

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
 
 import requests
 import sys
+import datetime
 
 args = sys.argv
 
 if len(args) == 1:
-    gameId = "210823"
+    gameId = "250823"
 else:
     gameId = args[1]
+
+formated_date = gameId[:4] + '20' + gameId[4:]
+date_str = datetime.datetime.strptime(formated_date, "%d%m%Y").strftime("%d %B %Y")
 
 options = Options()
 options.headless = False
@@ -30,7 +35,7 @@ force = find_between(gameData, "force:\"", "\",\\r")
 driver = Firefox(options=options)
 driver.get(f"https://rcijeux.fr/game/20minutes/mfleches?id={gameId}")
 
-title = driver.find_element(By.ID, "game-name")
-driver.execute_script(f"document.getElementById('game-name').innerHTML = '{title.text} - Force {force}';")
+title_element = driver.find_element(By.ID, "game-name")
+driver.execute_script(f"document.getElementById('game-name').innerHTML = '{date_str} - Force {force}';")
 
 # driver.close()
