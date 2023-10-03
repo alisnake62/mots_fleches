0a0c4a0b467f13ed09dcd281819a03b209501e37
Lucas Chaigne <lucas.chaigne@ovhcloud.com>
2023-10-03 18:06:16 +0200

    ajout du comic et d'autres trucs cools

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ args = sys.argv
 input_date = None if len(args) == 1 else args[1] # format ddmmyyyy
 
 input_date = datetime.datetime.strptime(input_date, "%d%m%Y") if input_date is not None else datetime.datetime.today()
-
+second_input_date = datetime.datetime.now().strftime("%Y/%m/%d")
 formated_date = input_date.strftime("%d%m%Y")
 game_id = formated_date[:4] + formated_date[6:]
 
@@ -20,7 +20,7 @@ raw_html = get_mots_fleches_html_raw(game_id)
 
 mf_3mn = generate_3mn_mf_from_raw(raw_html, force)
 
-horoscope = get_horoscope()
+horoscope = get_horoscope(second_input_date)
 
 full_3mn = get_full_3mn(mf_3mn, horoscope)
 
