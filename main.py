b145489f430fe8adcb75b5b2d3107eaa052255ed
Lucas Chaigne <lucas.chaigne@ovhcloud.com>
2023-11-09 07:52:22 +0100

    ciao l'artiste

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ parser = argparse.ArgumentParser(description="Description de votre script")
 parser.add_argument('--date_mots_fleches', type=str, default=datetime.datetime.today().strftime("%d%m%Y"), help="date au format ddmmyyyy")
 parser.add_argument('--date_comic', type=str, default=datetime.datetime.today().strftime("%d%m%Y"), help="date au format ddmmyyyy")
 parser.add_argument('--pic_path', type=str, default='central.png', help="chemin de la photo centrale des mots fleches")
-parser.add_argument('--comics', nargs='+', default=['pickles', 'garfield'], help="liste des comics à afficher")
+parser.add_argument('--comics', nargs='+', default=['pickles', 'crabgrass'], help="liste des comics à afficher")
 
 args = parser.parse_args()
 
