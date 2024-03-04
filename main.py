4a72cd5d6b47511e03f47ed6feba254aaab5e518
clement <clementsergent@yahoo.fr>
2024-03-04 10:35:14 +0100

    add logs

--- main.py
+++ main.py
@@ -14,8 +14,15 @@ pic_path = f'central_{mots_fleches_id}.png'
 img_txt = get_central_picture(mots_fleches_id)
 get_verso_picture(mots_fleches_id)
 
-central_picture_only = os.getenv("CENTRAL_PICTURE_ONLY", "0")
-if central_picture_only != '1':
+print(str(datetime.datetime.now()))
+
+nextcloud_files_only = os.getenv("NEXTCLOUD_FILES_ONLY", "0")
+
+print(f"nextcloud_files_only: {nextcloud_files_only}")
+
+if nextcloud_files_only != '1':
+
+    print("Get Full 3min")
     full_3mn = get_full_3mn(mots_fleches_id, pic_path)
 
     with open("html/index.html", "w", encoding="utf-8") as file:
